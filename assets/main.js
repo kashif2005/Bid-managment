19e1c1ea00f496fbfcca2212554b2b24a52c20fa
Kashif Khan <kk2005@gmail.com>
2024-02-29 12:52:29 +0500

    bidmanagment

---
+++ assets/main.js
@@ -0,0 +1,11 @@
+  // let click = document.getElementById('click');
+  let resetPage = document.getElementById('reset-page'); // Assuming there's only one element with the class 'reset-page'
+
+  // Define the onClick function
+  function onClick() {
+      resetPage.classList.remove('display-box');
+  }
+
+  function onlick() {
+      resetPage.classList.add('display-box');
+  }
